e2677236d5d08611985085e979e60b3bf2379da2
julia.R <julia.ristic@gmail.com>
2019-05-23 19:49:05 +0200

    ajout du carroussel

---
+++ view/home.php
@@ -0,0 +1,26 @@
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="public/img/fire1.jpg" class="d-block w-100" alt="fireman">
+    </div>
+    <div class="carousel-item">
+      <img src="public/img/fire2.jpg" class="d-block w-100" alt="fireman>
+    </div>
+    <div class="carousel-item">
+      <img src="public/img/fire3.jpg" class="d-block w-100" alt="fireman">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Precedent</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Suivant</span>
+  </a>
+</div>
