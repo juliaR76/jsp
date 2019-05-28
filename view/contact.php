63ae8b2c1f7b1c361866313f37b7021dd7ba076b
julia.R <julia.ristic@gmail.com>
2019-05-28 12:36:32 +0200

    ajout du formulaire de contact

---
+++ view/contact.php
@@ -0,0 +1,19 @@
+<section id="contact">
+    <div class="container">
+        <form>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Objet</label>
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Objet">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlInput1">Email</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="form-group">
+                <label for="exampleFormControlTextarea1">Message</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div> 
+            <button type="submit" class="btn btn-dark">Envoyer</button>
+        </form>
+    </div>
+</section>
