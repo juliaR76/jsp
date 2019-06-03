708ec85157224a22fd0b0ac7c6f6f1833611a5f5
julia.R <julia.ristic@gmail.com>
2019-06-03 13:18:51 +0200

    modification html  du formulaire

--- view/connexion.php
+++ view/connexion.php
@@ -1,11 +1,20 @@
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Mot de passe</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <button type="submit" class="btn btn-primary">Connecter</button>
-</form>
+<section id="connectPage">
+    <div class="container">
+    <div class="row">
+    <div class="col-lg-5 col-md-10 mx-auto">
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Votre email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Mot de passe</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Votre mot de passe">
+            </div>
+            <button type="submit" class="btn btn-dark">Connecter</button>
+        </form>
+    </div>   
+    </div>
+    </div>
+</section>
+
