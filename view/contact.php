ef21558ead105e6c22048af6a66ea19cee7735e3
julia.R <julia.ristic@gmail.com>
2019-06-03 13:18:10 +0200

    modification html du formulaire

--- view/contact.php
+++ view/contact.php
@@ -1,19 +1,31 @@
 <section id="contact">
     <div class="container">
-        <form>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Objet</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Objet">
+        <div class="row">
+            <div class="col-lg-5 col-md-10 mx-auto">
+                <form>
+                    <div class="control-group">
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Objet</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Objet">
+                        </div>
+                    </div> 
+                    <div class="control-group">
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Email</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                        </div>
+                    </div>     
+                    <div class="control-group">
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">Message</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div> 
+                    </div> 
+                    <div class="form-group">   
+                        <button type="submit" class="btn btn-dark">Envoyer</button>
+                    </div>
+                </form>
             </div>
-            <div class="form-group">
-                <label for="exampleFormControlInput1">Email</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">Message</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div> 
-            <button type="submit" class="btn btn-dark">Envoyer</button>
-        </form>
+        </div>
     </div>
 </section>
