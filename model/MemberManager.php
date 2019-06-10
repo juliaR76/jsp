bf53aab8950c3eb32ea6c3dfac4c8fae659528c8
julia.R <julia.ristic@gmail.com>
2019-06-10 16:15:33 +0200

    mise a jour de la fonction add

--- model/MemberManager.php
+++ model/MemberManager.php
@@ -2,15 +2,15 @@
 
 class MemberManager
 {
-    private $_db;
+    private $db;
 
     public function __construct()
     {
         try
         {
-        $this->db = new PDO('mysql:host=localhost; dbname=jspvdc;charset=utf8', 'root', '');
+            $this->db = new PDO('mysql:host=localhost; dbname=jspvdc;charset=utf8', 'root', '');
         }
-        catch (exception $e)
+        catch(exception $e)
         {
             die('Erreur : '.$e->getMessage());
         }
@@ -18,26 +18,26 @@ class MemberManager
 
     public function add(Member $user)
     {
-
+        $req = $this->db->prepare('INSERT INTO member(pseudo, last_name, first_name, adresse, pass, email, phone, authorization, registration_date) VALUES (:pseudo, :last_name, :first_name, :adresse, :pass, :email, :phone, 0, CURDATE())');
+        $req->execute([
+            'pseudo' => $user->pseudo(),
+            'last_name' => $user->last_name(),
+            'first_name' => $user->first_name(),
+            'adresse' => $user->adresse(),
+            'pass' => $user->pass(), 
+            'email' => $user->email(),
+            'phone' => $user->phone()
+        ]);
     }
 
-    public function get()
+    public function get($pseudo, $pass)
     {
-
+        $req = $this->db->prepare('SELECT id, pseudo, last_name, first_name, adresse, pass, email, phone FROM member WHERE pseudo = :pseudo AND pass = :pass');
+        $req->execute([
+            'pseudo' => $pseudo,
+            'pass' => $pass
+        ]); 
+        $data = $req->fetch(PDO::FETCH_ASSOC);
+        return new Member($data);
     }
-
-    public function update()
-    {
-
-    }
-
-    public function delete()
-    {
-        
-    }
-
-
-
-
-
 }
