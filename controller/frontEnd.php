bd5a370986f18f1ade5c2bdf58d4e6c4d5212b39
julia.R <julia.ristic@gmail.com>
2019-07-10 14:26:15 +0200

    mise à jour + ajout du captcha

--- controller/frontEnd.php
+++ controller/frontEnd.php
@@ -1,140 +1,46 @@
 <?php
 
 
-function home()
-{
-    ob_start();
-    require('view/home.php');
-    $content = ob_get_clean(); 
-    require('template_Front.php');
-    
-}
+function home(){
 
-function infos()
-{
-    ob_start();
-    require('view/infos.php');
-    $content = ob_get_clean(); 
-    require('template_Front.php');
-}
-
-function inscription()
-{
-    if(isset($_POST['creerCompte']))
-    {
-        $pseudo = htmlspecialchars($_POST['pseudo']);
-        $last_name = htmlspecialchars($_POST['last_name']);
-        $first_name = htmlspecialchars($_POST['first_name']);
-        $adresse = htmlspecialchars($_POST['adresse']);
-        $pass = sha1($_POST['pass']);
-        $confirmationPass = sha1($_POST['confirmationPass']);
-        $email = htmlspecialchars($_POST['email']);
-        $phone = htmlspecialchars($_POST['phone']);
-
-        if(!empty($_POST['pseudo']) AND !empty($_POST['last_name']) AND !empty($_POST['first_name']) AND !empty($_POST['adresse']) AND !empty($_POST['pass']) AND !empty($_POST['confirmationPass']) AND !empty($_POST['email']) AND !empty($_POST['phone']))
-        {
-            if($_POST['pass'] == $_POST['confirmationPass'])
-            {
-                if(filter_var($email, FILTER_VALIDATE_EMAIL))
-                {
-                    $user = new Member([
-                    'pseudo' => $_POST['pseudo'],
-                    'last_name' => $_POST['last_name'],
-                    'first_name' => $_POST['first_name'],
-                    'adresse' => $_POST['adresse'],
-                    'pass' => sha1($_POST['pass']),
-                    'email' => $_POST['email'],
-                    'phone' => $_POST['phone']
-                    ]);
-                    $memberManager = new MemberManager;
-                    $member = $memberManager->add($user);
-                    header('location: index.php?action=connexion');
-                }
-                else
-                {
-                    $erreur ='<html>
-                    <body>
-                        <div class="alert alert-danger" role="alert">
-                            Votre email est invalide!
-                        </div>
-                    </body>
-                    </html>'; 
-                }
-            }
-            else
-            {
-                $erreur ='<html>
-                        <body>
-                            <div class="alert alert-danger" role="alert">
-                                Vos mots de pass ne corresponde pas!
-                            </div>
-                        </body>
-                        </html>';
-            }
-        }
-        else
-            {
-                $erreur ='<html>
-                        <body>
-                            <div class="alert alert-danger" role="alert">
-                                Attention de bien remplir tout les champs !
-                            </div>
-                        </body>
-                        </html>';
-            }         
-    }
-
-    ob_start();
-    require('view/inscription.php');
-    $content = ob_get_clean(); 
-    require('template_Front.php');
-}
-
-function connexion()
-{   
-    if(isset($_POST['connecter']))
-    {
-        $pseudo = htmlspecialchars($_POST['pseudo']);
-        $pass = sha1($_POST['pass']);
-        
-        if(!empty($_POST['pseudo']) AND !empty($_POST['pass']))
-        {
-            $memberManager = new MemberManager;
-            $member = $memberManager->get($_POST['pseudo'], sha1($_POST['pass']));
-            $_SESSION['id'] = $member->id();
-            $_SESSION['pseudo'] = $_POST['pseudo'];
-            $_SESSION['email'] = $member->email();
-            $_SESSION['last_name'] = $member->last_name();
-            $_SESSION['first_name'] = $member->first_name();
-            $_SESSION['adresse'] = $member->adresse();
-            $_SESSION['phone'] = $member->phone();
-            header('location: index.php?action=admin');
-        } 
-        else
-        {
-            $erreur ='<html>
-                        <body>
-                            <div class="alert alert-danger" role="alert">
-                                Attention de bien remplir tout les champs!
-                            </div>
-                        </body>
-                     </html>';                       
-        }
-           
-    }
- 
-    ob_start();
-    require('view/connexion.php');
-    $content = ob_get_clean(); 
-    require('template_Front.php');
-}
-
-function contact()
-{
     if(isset($_POST['sendMail']))
     {
-        if(!empty($_POST['object']) AND !empty($_POST['email']) AND !empty($_POST['message']))
+
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+        $data = [
+            'secret' => "6LeS4qwUAAAAAITou3lReGOb7qef-pALA3y1aIyW",
+            'response' => $_POST['token'],
+            'remoteip' => $_SERVER['REMOTE_ADDR']
+            ];
+
+        $options = array(
+            'http' => array(
+            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+            'method'  => 'POST',
+            'content' => http_build_query($data)
+            )
+        );
+
+        $context  = stream_context_create($options);
+        $response = file_get_contents($url, false, $context);
+        $res = json_decode($response, true);
+
+        if($res['success'] == true) {
+                // Perform you logic here for ex:- save you data to database
+            $erreur = '<div class="alert alert-success">
+                            <strong>Success!</strong> VOtre message à bien été envoyé.
+                        </div>';
+        } else {
+            $erreur = '<div class="alert alert-warning">
+                            <strong>Error!</strong> Vous n\'êtes pas un humain.
+                        </div>';
+        }
+
+        
+        if(!empty($_POST['prenom']) AND !empty($_POST['nom']) AND !empty($_POST['object']) AND !empty($_POST['email']) AND !empty($_POST['message']))
         {
+            
+
             $header="MIME-version: 1.0\r\n";
             $header.='From:"PrimFX.com"<support@jeanf.com>'."\n";
             $header.='Content-Type:text/html; charset="utf-8"'."\n";
@@ -143,7 +49,8 @@ function contact()
             $message=
                 '<html>
                     <body>
-                        <div align="center">   
+                        <div align="center"> 
+                            <u>Nom et prenom de l\'expéditeur :</u>'.$_POST['nom'].' '.$_POST['prenom'].'<br />  
                             <u>objet de l\'expéditeur :</u>'.$_POST['object'].'<br />
                             <u>Mail de l\'expéditeur :</u>'.$_POST['email'].'<br />
                             <br />
@@ -153,7 +60,6 @@ function contact()
                 </html>';
 
              mail("julia.ristic@gmail.com", "Contact - Mon site", $message, $header);
-             echo "<script>window.alert(\"Votre message a bien été envoyé !\")</script>";
         }
         else
         {
@@ -165,13 +71,52 @@ function contact()
                         </body>
                         </html>';
         }
-    } 
-    ob_start();
-    require('view/contact.php');
-    $content = ob_get_clean(); 
-    require('template_Front.php');    
+
+
+	}
+
+    require('template.php');
 }
 
-
-
-
+function connexion()
+{   
+    if(isset($_POST['connecter']))
+    {
+        $pseudo = htmlspecialchars($_POST['pseudo']);
+        
+        
+        if(!empty($_POST['pseudo']) || !password_verify($_POST['pass'], $data) )
+        {
+            
+            $memberManager = new MemberManager;
+            $member = $memberManager->get($_POST['pseudo']);
+            
+            $_SESSION['id'] = $member->id();
+            $_SESSION['avatar'] = $member->avatar();
+            $_SESSION['pseudo'] = $_POST['pseudo'];
+            $_SESSION['email'] = $member->email();
+            $_SESSION['last_name'] = $member->last_name();
+            $_SESSION['first_name'] = $member->first_name();
+            $_SESSION['adresse'] = $member->adresse();
+            $_SESSION['phone'] = $member->phone();
+            $_SESSION['authorization'] = $member->authorization();
+            
+            header('location: index.php?action=admin');
+        } 
+        else
+        {
+            $erreur ='<html>
+                        <body>
+                            <div class="alert alert-danger" role="alert">
+                                Attention de bien remplir tout les champs!
+                            </div>
+                        </body>
+                     </html>';                       
+        }       
+    }
+    
+    ob_start();
+    require('view/connexion.php');
+    $content = ob_get_clean(); 
+    require('templatePage.php');
+}
