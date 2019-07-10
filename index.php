81d1e8b41894611b50ae424701e1b5e63cb2398e
julia.R <julia.ristic@gmail.com>
2019-07-10 14:36:25 +0200

    mise à jour du fichier

--- index.php
+++ index.php
@@ -1,39 +1,78 @@
 <?php
 
+session_start();
+
+require('model/Lesson.php');
+require('model/LessonManager.php');
+require('model/Member.php');
+require('model/MemberManager.php');
 require('controller/frontEnd.php');
 require('controller/backend.php');
+
 
 if(empty($_SERVER['QUERY_STRING']))
 {
     
     home();
-}else{
-    if(isset($_GET['action']))
-    {
-        if($_GET['action'] == 'home')
-        {  
-                home();
-            
-        }
-        elseif($_GET['action'] == 'infos')
-        {  
-                infos();
-            
-        }
-        elseif($_GET['action'] == 'contact')
-        {  
-                contact();
-            
-        }
-        elseif($_GET['action'] == 'connexion')
-        {  
-                connexion(); 
-        }
-        elseif($_GET['action'] == 'admin')
+}else
+{
+        if(isset($_GET['action']))
         {
-                admin();
+                if($_GET['action'] == 'home')
+                {  
+                        home();
+                
+                }
+                elseif($_GET['action'] == 'connexion')
+                {  
+                        connexion(); 
+
+                }
+                elseif($_GET['action'] == 'admin')
+                {
+                        admin();
+                }
+                elseif($_GET['action'] == 'addPost')
+                {  
+                        admin();
+                
+                }
+                elseif($_GET['action'] == 'addMember')
+                {
+                        admin();
+                }  
+                elseif($_GET['action'] == 'update')
+                {  
+                        update();
+                
+                }              
+                elseif($_GET['action'] == 'lessons')
+                {
+                        lessons();
+                }
+                elseif($_GET['action'] == 'lesson')
+                {
+                        lesson();     
+                        
+                }
+                elseif($_GET['action'] == 'delete')
+                {  
+                        delete();
+                
+                }
+                elseif($_GET['action'] == 'deconnexion')
+                {
+                        deconnexion();
+                }
+                elseif($_GET['action'] == 'erreur')
+                {
+                        erreur();
+                }else{
+                        erreur();
+                }    
+        }else{
+                header('location: index.php?action=erreur');
         }
-    }
 }
 
 
