9796633b897c0cc5aa9b97e298357c41e0e79fc8
julia.R <julia.ristic@gmail.com>
2019-07-10 14:38:31 +0200

    ajout d'un membre

---
+++ view/addMember.php
@@ -0,0 +1,58 @@
+        
+             <form action="index.php?action=addMember" method="post" name="sentMessage" id="contactForm" novalidate>
+             <?php if(isset($erreur)){ echo $erreur;} ?>
+                <div class="control-group ">
+                    <div class="form-group floating-label-form-group controls">
+                        <label>Pseudo</label>
+                        <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" id="pseudo" value="<?php if(isset($pseudo)){ echo $pseudo; } ?>">
+                    </div>
+                </div>
+                <div class="control-group ">
+                    <div class="form-group floating-label-form-group controls">
+                        <label>Nom</label>
+                        <input type="text" name="last_name" class="form-control" placeholder="nom de famille" id="last_name" value="<?php if(isset($last_name)){ echo $last_name; } ?>">
+                    </div>
+                </div>
+                <div class="control-group ">
+                    <div class="form-group floating-label-form-group controls">
+                        <label>Prenom</label>
+                        <input type="text" name="first_name" class="form-control" placeholder="nom de famille" id="first_name" value="<?php if(isset($first_name)){ echo $first_name; } ?>">
+                    </div>
+                </div>
+                <div class="control-group ">
+                    <div class="form-group floating-label-form-group controls">
+                        <label>Adresse</label>
+                        <input type="text" name="adresse" class="form-control" placeholder="Adresse postale" id="adresse" value="<?php if(isset($adresse)){ echo $adresse; } ?>">
+                    </div>
+                </div>
+                <div class="control-group ">
+                    <div class="form-group floating-label-form-group controls">
+                        <label>Mot de pass</label>
+                        <input type="password" name="pass" placeholder="mot de passe" class="form-control" id="pass">
+                    </div>
+                </div>
+                <div class="control-group ">
+                    <div class="form-group floating-label-form-group controls">
+                        <label>Confirmer votre mot de pass</label>
+                        <input type="password" name="confirmationPass" placeholder="confirmer mot de passe" class="form-control" id="pass">
+                    </div>
+                </div>
+                <div class="control-group ">
+                    <div class="form-group floating-label-form-group controls">
+                        <label>Email</label>
+                        <input type="email" name="email" class="form-control" placeholder="Email" id="Email" value="<?php if(isset($email)){ echo $email; } ?>">
+                    </div>
+                </div>
+                <div class="control-group ">
+                    <div class="form-group floating-label-form-group controls">
+                        <label>numero de telephone</label>
+                        <input type="text" name="phone" class="form-control" placeholder="n° de fixe ou portable" id="phone" value="<?php if(isset($phone)){ echo $phone; } ?>">
+                    </div>
+                </div> 
+                <br>
+                <div id="success"></div>
+                    <div class="form-group">
+                    <a href="index.php?action=admin"><button type="submit" name="creerCompte" class="btn btn-primary">Créer un compte</button></a>
+                </div>
+            </form>
+
