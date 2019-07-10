d859d5ddaf529f96cb8c6177cdf0b2f55ea1b826
julia.R <julia.ristic@gmail.com>
2019-07-10 14:44:00 +0200

    page info suppression

---
+++ view/delete.php
@@ -0,0 +1,11 @@
+<!-- message de supression du billet  -->
+<div class="card text-center">
+    <div class="card-header">
+        Info
+    </div>
+    <div class="card-body">
+        <h5 class="card-title">Juste pour te dire...</h5>
+        <p class="card-text">Le cours à bien été supprimé.</p>
+        <a href="index.php?action=admin" class="btn btn-primary">Retour au Tableau de Bord</a>
+    </div>
+</div>
